2b3e77041eb218c305d6271aa67bac7915c05776
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-02 13:08:54 +0100

    feat(Greeter): add

---
+++ src/lib/Greeter/Greeter.spec.ts
@@ -0,0 +1,21 @@
+import { fireEvent, render, screen } from '@testing-library/svelte'
+
+import Greeter from './Greeter.svelte'
+
+describe('Greeter', () => {
+    test('should render', () => {
+        render(Greeter)
+
+        expect(screen.getByRole('textbox')).toBeTruthy()
+    })
+
+    test('should greet when input', async () => {
+        render(Greeter)
+
+        const input = screen.getByRole('textbox')
+
+        await fireEvent.input(input, { target: { value: 'world' } })
+
+        expect(screen.getByText('Hello, world!')).toBeTruthy()
+    })
+})
---
+++ src/lib/Greeter/Greeter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let value: string = ''
+</script>
+
+<input type="text" bind:value placeholder="Type your name" />
+
+{#if value}
+    <p>Hello, {value}!</p>
+{/if}
