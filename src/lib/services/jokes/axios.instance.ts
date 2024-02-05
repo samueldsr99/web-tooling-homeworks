85e1eed26f715d1c1144cdf88647af33ba1aa42b
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-05 10:15:27 +0100

    bugfix: change .env variables to be reached on client

--- src/lib/services/jokes/axios.instance.ts
+++ src/lib/services/jokes/axios.instance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const jokesInstance = axios.create({
-    baseURL: import.meta.env.JOKES_API_BASE_URL,
+    baseURL: import.meta.env.VITE_JOKES_API_BASE_URL,
     headers: {
-        'X-Api-Key': import.meta.env.JOKES_API_KEY,
+        'X-Api-Key': import.meta.env.VITE_JOKES_API_KEY,
     },
 })
 
