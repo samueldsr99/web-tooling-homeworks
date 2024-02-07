d6f763830bdcca779f7e88c83270605e82e8be32
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-07 12:23:30 +0100

    test buggy pr

--- src/lib/services/jokes/axios.instance.ts
+++ src/lib/services/jokes/axios.instance.ts
@@ -1,10 +1,19 @@
 import axios from 'axios'
 
-const jokesInstance = axios.create({
-    baseURL: import.meta.env.VITE_JOKES_API_BASE_URL,
-    headers: {
+    const jokesInstance = axios.create({
+                    baseURL: import.meta.env.VITE_JOKES_API_BASE_URL,
+            headers: {
         'X-Api-Key': import.meta.env.VITE_JOKES_API_KEY,
-    },
+},
 })
 
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+sum("asd", 1);
+
 export default jokesInstance
+
+            console.log('LOL')
