562614cbd79c99fd03d4778435760e747ff00cd3
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-13 11:42:11 +0100

    chore: include `tailwind` within stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,16 @@ import type { StorybookConfig } from '@storybook/svelte-vite'
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        '@storybook/addon-styling-webpack'
-    ],
+
     framework: {
         name: '@storybook/svelte-vite',
         options: {},
     },
+
     docs: {
         autodocs: 'tag',
     },
+
+    addons: ['@storybook/addon-styling-webpack']
 }
 export default config
