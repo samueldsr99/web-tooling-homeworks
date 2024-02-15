9da6939a7f0b446c435cc3fd8d3e94dac15959f2
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-15 22:29:10 +0100

    feat(joke-card): add

---
+++ src/lib/components/jokes/joke-card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let content: {
+    first: string
+    second?: string
+  }
+  export let category: string
+</script>
+
+<div class="max-w-[398px] rounded-lg border border-border bg-card p-2">
+  <div>
+    <p class="text-base font-medium">{content.first}</p>
+    <p class="text-sm font-medium text-slate-500">{content?.second ?? ''}</p>
+  </div>
+
+  <div class="flex items-center justify-end">
+    <span class="text-sm font-medium text-slate-400">{category}</span>
+  </div>
+</div>
