02600dbf7feee3035ce96f0f834591960d95c3e4
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-15 11:10:48 +0100

    test(button): add

---
+++ src/lib/components/ui/button/button.spec.ts
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/svelte'
+import { vi } from 'vitest'
+
+import ButtonView from './buttonview.svelte'
+
+describe('ButtonView', () => {
+    test('should render', () => {
+        render(ButtonView)
+
+        const button = screen.getByRole('button')
+
+        expect(button).toBeTruthy()
+    })
+
+    test('should render slot text', () => {
+        render(ButtonView, {})
+
+        const button = screen.getByRole('button')
+
+        expect(button.textContent).toBe('Hello')
+    })
+})
