8486ef5f994ff0c6f6d3eab4f2052d2a45e63063
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-15 23:39:34 +0100

    bugfix: fix word wraps errors

--- src/lib/components/jokes/joke-card.svelte
+++ src/lib/components/jokes/joke-card.svelte
@@ -6,8 +6,10 @@
   export let category: string
 </script>
 
-<div class="max-w-[398px] rounded-lg border border-border bg-card p-2">
-  <div>
+<div
+  class="mx-auto flex min-h-[140px] w-full max-w-[398px] flex-col justify-between rounded-lg border border-border bg-card p-2 md:mx-0"
+>
+  <div class="whitespace-pre-wrap break-words">
     <p class="text-base font-medium">{content.first}</p>
     <p class="text-sm font-medium text-slate-500">{content?.second ?? ''}</p>
   </div>
