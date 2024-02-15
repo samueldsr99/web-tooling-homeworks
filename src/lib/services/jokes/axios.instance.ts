77e1c6d6e6cd7b68b614dda9d2c1cf4d86f64907
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-16 00:19:11 +0100

    chore: add cors headers to axios instance

--- src/lib/services/jokes/axios.instance.ts
+++ src/lib/services/jokes/axios.instance.ts
@@ -3,7 +3,10 @@ import axios from 'axios'
 const jokesInstance = axios.create({
   baseURL: import.meta.env.VITE_JOKES_API_BASE_URL,
   headers: {
-    'X-Api-Key': import.meta.env.VITE_JOKES_API_KEY,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
   },
 })
 
